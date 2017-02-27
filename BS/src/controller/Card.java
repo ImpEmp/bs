667fb302b7ad9b095e6c3c2c171be250bc5c6218
Cameron Jenkins <cameron000@me.com>
2017-02-26 19:47:25 -0500

    Add files via upload

--- BS/src/controller/Card.java
+++ BS/src/controller/Card.java
@@ -9,9 +9,7 @@ public class Card {
 	public int getNumber() {
 		return n;
 	}
-	
 	public String toString() {
 		return String.valueOf(n);
 	}
-
 }
--- BS/src/controller/Player.java
+++ BS/src/controller/Player.java
@@ -51,4 +51,3 @@ public abstract class Player {
 		return hand.toString();
 	}
 }
-
